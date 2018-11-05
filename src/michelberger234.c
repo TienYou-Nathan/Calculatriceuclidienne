24f33918f198096a290d5c7422399c737256c85e
Arthu <Arthu@MSI-Arthur.mshome.net>
2018-11-05 09:15:44 +0100

    created project

---
+++ src/michelberger234.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : michelberger234.c
+ Author      : Arthur Coppey
+ Version     :
+ Copyright   : none
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts(""); /* prints  */
+	return EXIT_SUCCESS;
+}
